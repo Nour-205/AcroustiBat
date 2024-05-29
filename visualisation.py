7f32260b0c88472b8a928f9bac2bf7c59756c2db
rime3-14 <rim.chihab-eddine@insa-lyon.fr>
2024-05-29 18:53:11 +0200

    added a graphic window

--- visualisation.py
+++ visualisation.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.interpolate import griddata
 from tkinter import Tk, Frame, Button
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
 
 
 
@@ -42,32 +43,67 @@ Xi, Yi = np.meshgrid(xi, yi)
 #méthode cubique pour interpolation (courbe de surface continue) + turning Z into a 2D element 
 Zi = griddata((x, y), z, (Xi, Yi), method='cubic')
 
+def create3dplot():
 
-fig = plt.figure()
-axe = fig.add_subplot(111, projection='3d')
-surface = axe.plot_surface(Xi, Yi, Zi, cmap=cm.coolwarm)
-#visualisation des points bruts pour comparer ? (removable feature)
-axe.scatter(x, y, z, color='r', s=10, alpha = 0.5)
+    fig = plt.figure()
+    axe = fig.add_subplot(111, projection='3d')
+    surface = axe.plot_surface(Xi, Yi, Zi, cmap=cm.coolwarm)
+    #visualisation des points bruts pour comparer ? (removable feature)
+    axe.scatter(x, y, z, color='r', s=10, alpha = 0.5)
 
-fig.colorbar(surface, shrink=0.5, aspect=5)
-plt.xlabel("coordonnée x")
-plt.ylabel("coordonnée y")
-plt.clabel("amplitude")
-plt.show()
+    fig.colorbar(surface, shrink=0.5, aspect=5)
+    axe.set_xlabel("coordonnée x")
+    axe.set_ylabel("coordonnée y")
+    axe.set_zlabel("amplitude")
+    axe.set_title("Title of the Third Ax")
+    return fig
 
 
-fig = plt.figure()
-fig.suptitle('boxplot de la gamme de fréquences', fontsize=14, fontweight='bold')
+def createboxplot():
+    
+    fig = plt.figure()
+    fig.suptitle('boxplot de la gamme de fréquences', fontsize=14, fontweight='bold')
 
-ax = fig.add_subplot(111)
-c = 'blue'
-box = ax.boxplot(z, patch_artist = True)
+    ax = fig.add_subplot(111)
+    c = 'blue'
+    box = ax.boxplot(z, patch_artist = True)
+    colors = ['lightblue', 'lightgreen']
+    for patch, color in zip(box['boxes'], colors):
+        patch.set_facecolor(color)
+    ax.set_ylabel('amplitudes')
+    return fig
 
-colors = ['lightblue', 'lightgreen']
-for patch, color in zip(box['boxes'], colors):
-    patch.set_facecolor(color)
+class fenetre(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("Visualisation")
+        self.geometry("800x600")
+        frame = tk.Frame(self)
+        frame.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.TRUE)
+        self.plot_frame = tk.Frame(frame)
+        self.plot_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.TRUE)
 
-ax.set_ylabel('amplitudes')
+        Button1 = Button(frame, text="Show 3D Plot", command=self.plot3d, width=20, height=2, bg='lightpink')
+        Button1.pack(side="left", padx=20, pady=20, expand=True)
+        Button2 = Button(frame, text="Show Box Plot", command=self.plotbox, width=20, height=2, bg="lightblue")
+        Button2.pack(side="left", padx=20, pady=20, expand=True)
 
-plt.show()
+    def plot3d(self):
+        for widget in self.plot_frame.winfo_children():
+            widget.destroy()
+        fig = create3dplot()
+        canvas = FigureCanvasTkAgg(fig, master=self.plot_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
+    def plotbox(self):
+        for widget in self.plot_frame.winfo_children():
+            widget.destroy()
+        fig = createboxplot()
+        canvas = FigureCanvasTkAgg(fig, master=self.plot_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+
+app = fenetre()
+app.mainloop()
